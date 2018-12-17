c2f219070c628672d2b1d0a9b0b95b21b9e00fec
yingming.han@51zouchuqu.com <yingming.han@51zouchuqu.com>
2018-12-17 16:24:14 +0800

    引入taro-ui的tab切换

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import '@tarojs/async-await'
 
 import Taro, {Component} from '@tarojs/taro'
-import Home from './pages/home'
+import Index from './pages/index'
 import dva from './dva'
 import models from './model'
 import {Provider} from '@tarojs/redux'
+
 import './styles/base.scss'
 
 
@@ -19,11 +20,14 @@ const store = dvaApp.getStore();
 
 class App extends Component {
 
+
+
   config = {
     pages: [
-      'pages/home/index',
-      'pages/discovery/index',
-      'pages/more/index',
+      'pages/index/index',
+      // 'pages/home/index',
+      // 'pages/discovery/index',
+      // 'pages/more/index',
     ],
     window: {
       backgroundTextStyle: 'light',
@@ -32,29 +36,29 @@ class App extends Component {
       navigationBarTextStyle: 'white',
       enablePullDownRefresh: true
     },
-    tabBar: {
-      color: "#626567",
-      selectedColor: "#2A8CE5",
-      backgroundColor: "#FBFBFB",
-      borderStyle: "white",
-      list: [{
-        pagePath: "pages/home/index",
-        text: "首页",
-        iconPath: "./asset/images/index.png",
-        selectedIconPath: "./asset/images/index_focus.png"
-      }, {
-        pagePath: "pages/discovery/index",
-        text: "发现",
-        iconPath: "./asset/images/discovery.png",
-        selectedIconPath: "./asset/images/discovery_focus.png"
-      },
-        {
-          pagePath: "pages/more/index",
-          text: "我的",
-          iconPath: "./asset/images/burger.png",
-          selectedIconPath: "./asset/images/burger_focus.png"
-        }]
-    }
+    // tabBar: {
+    //   color: "#626567",
+    //   selectedColor: "#2A8CE5",
+    //   backgroundColor: "#FBFBFB",
+    //   borderStyle: "white",
+    //   list: [{
+    //     pagePath: "pages/home/index",
+    //     text: "首页",
+    //     iconPath: "./asset/images/index.png",
+    //     selectedIconPath: "./asset/images/index_focus.png"
+    //   }, {
+    //     pagePath: "pages/discovery/index",
+    //     text: "发现",
+    //     iconPath: "./asset/images/discovery.png",
+    //     selectedIconPath: "./asset/images/discovery_focus.png"
+    //   },
+    //     {
+    //       pagePath: "pages/more/index",
+    //       text: "我的",
+    //       iconPath: "./asset/images/burger.png",
+    //       selectedIconPath: "./asset/images/burger_focus.png"
+    //     }]
+    // }
   }
 
   componentDidMount() {
@@ -71,8 +75,9 @@ class App extends Component {
   }
 
   render() {
+
     return (<Provider store={store}>
-      <Home/>
+      <Index/>
       </Provider>);
   }
 }
